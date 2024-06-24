4a785afc26a07b6d48ebafd810b46cfec3243caa
JohnInWI <psigncrafters@gmail.com>
2024-06-24 04:30:14 -0500

    Update 0.setup.js

--- 0.setup.js
+++ 0.setup.js
@@ -1,8 +1,7 @@
-const robotNumber = '402'; //change to your robot number- retain the quotation marks!
+const robotNumber = '400'; //change to your robot number- retain the quotation marks!
 
-const defaultScreen = '2'; //1=home, 2=tasks,3=sandbox 4=joystick
+const defaultScreen = '2'; //1=home, 2=tasks,3=teachable 4=joystick
 
 var temp = 0;
 var hum = 0;
-         
   
